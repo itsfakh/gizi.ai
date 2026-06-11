c7266f7450eaf5a4863c298a57dad0683e89b387
itsfakh <fakhriitsnanto357@gmail.com>
2026-06-11 13:12:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -105,18 +105,6 @@ st.markdown("""
 
 </style>
 """, unsafe_allow_html=True)
-p,
-li,
-label,
-span,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-    color: #111827 !important;
-}
 
 # ==================================
 # HEADER
